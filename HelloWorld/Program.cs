79e9795a10f43bf08f5ed4e8435c98d005251234
MarcDevGuy127 <marceloh.github@gmail.com>
2026-04-17 21:19:31 -0300

    feat: add two print lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
+
+// Write code in the C# Code Editor to display two messages
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
