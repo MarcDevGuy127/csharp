c1e9d217dc86c694ee8ff8a6c361a889edbcafba
MarcDevGuy127 <marceloh.github@gmail.com>
2026-04-17 22:30:57 -0300

    test: add print methods tests

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,18 @@
 ﻿Console.WriteLine("Hello, World!");
 
+// CHALLENGE
 // Write code in the C# Code Editor to display two messages
 Console.WriteLine("This is the first line.");
 Console.WriteLine("This is the second line.");
+
+// WriteLine() X Write()
+
+// using WriteLine() method, appends lines after print
+Console.WriteLine("1");
+Console.WriteLine("2");
+Console.WriteLine("3");
+
+// using Write() method, print in line
+Console.Write("1");
+Console.Write("2");
+Console.Write("3");
