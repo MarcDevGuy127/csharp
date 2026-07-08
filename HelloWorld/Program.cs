9811ef015e097bf51bf7709d79b5645feacef3e8
MarcDevGuy127 <marceloh.github@gmail.com>
2026-07-07 21:24:05 -0300

    feat: add Write and WriteLine lesson

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // WriteLine creates a new line at the end
+            // Write don't creates a new line
+            Console.Write("Hello World! with Write");
+            Console.WriteLine("Hello World! with WriteLine");
+            Console.Write("Hello World! with Write");
+
+            // You cam also do aritmetic operations like:
+            Console.WriteLine(6+6);
         }
     }
 }
