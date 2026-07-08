6e013045d1d16d4f2b663328cec295367ec73979
MarcDevGuy127 <marceloh.github@gmail.com>
2026-07-08 18:56:23 -0300

    feat: add const and new variables

--- HelloWorld/Fundamentals/Variables.cs
+++ HelloWorld/Fundamentals/Variables.cs
@@ -8,18 +8,25 @@ namespace CSharpStudies.Fundamentals
     {
         public static void Run()
         {
+            int x, y, z;
+            x = y = z = 50; // same value for these variables
             string name = "Marcelo";
             int age = 20;
             double myMoney = 278.99D;
+            long pocket = 15000000000L;
             char firstLetter = 'M';
             bool myBool = true;
+            const string university = "UNIBRASIL";
 
             Console.WriteLine("Variables example");
             Console.WriteLine(name);
+            Console.WriteLine(university);
             Console.WriteLine(age);
             Console.WriteLine(myMoney);
+            Console.WriteLine(pocket);
             Console.WriteLine(firstLetter);
             Console.WriteLine(myBool);
+            Console.WriteLine(x + y + z);
         }
     }
 }
