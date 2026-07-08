1eb024c59c50f17a0c8ff9e7e661dafe5ecaa481
MarcDevGuy127 <marceloh.github@gmail.com>
2026-07-07 23:10:58 -0300

    feat: add Variables lesson

---
+++ HelloWorld/Fundamentals/Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Fundamentals
+{
+    public class Variables
+    {
+        public static void Run()
+        {
+            string name = "Marcelo";
+            int age = 20;
+            double myMoney = 278.99D;
+            char firstLetter = 'M';
+            bool myBool = true;
+
+            Console.WriteLine("Variables example");
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(myMoney);
+            Console.WriteLine(firstLetter);
+            Console.WriteLine(myBool);
+        }
+    }
+}
